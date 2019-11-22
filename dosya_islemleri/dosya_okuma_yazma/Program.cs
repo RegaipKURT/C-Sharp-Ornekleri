ecafb93941662e9d72ad2a2eee0e68a11fa66870
RegaipKurt <regkrt@gmail.com>
2019-11-23 01:55:59 +0300

    Dosya İşlemleri

---
+++ dosya_islemleri/dosya_okuma_yazma/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosya_okuma_yazma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dosyayaYaz();
+            dosyadanOku();
+        }
+        private static void dosyayaYaz()
+        {
+            string dosya_yolu = @"C:\Users\SeBo-\Desktop\kayıt.py";
+            //İşlem yapacağımız dosyanın yolunu belirtiyoruz.
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
+            //2.parametre dosya varsa açılacağını yoksa oluşturulacağını belirtir,
+            //3.parametre dosyaya erişimin veri yazmak için olacağını gösterir.
+            StreamWriter sw = new StreamWriter(fs);
+            //Yazma işlemi için bir StreamWriter nesnesi oluşturduk.
+            sw.WriteLine("import tensorflow as tf");
+            sw.WriteLine("import pandas as pd\nimportnumpy as np\nimport matplotlib.pyplot as plt");
+            //Dosyaya ekleyeceğimiz iki satırlık yazıyı WriteLine() metodu ile yazacağız.
+            sw.Flush();
+            //Veriyi tampon bölgeden dosyaya aktardık.
+            sw.Close();
+            fs.Close();
+            //İşimiz bitince kullandığımız nesneleri iade ettik.
+        }
+        public static void dosyadanOku()
+        {
+            string dosya_yolu = @"C:\Users\SeBo-\Desktop\Filmler.txt";
+            //Okuma işlem yapacağımız dosyanın yolunu belirtiyoruz.
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
+            //2.parametre dosyanın açılacağını,
+            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
+            StreamReader sw = new StreamReader(fs);
+            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            string yazi = sw.ReadLine();
+            while (yazi != null)
+            {
+                Console.WriteLine(yazi);
+                yazi = sw.ReadLine();
+            }
+            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
+            //Son satır okunduktan sonra okuma işlemini bitirdik
+            sw.Close();
+            fs.Close();
+            //İşimiz bitince kullandığımız nesneleri iade ettik.
+            Console.ReadLine();
+        }
+    }
+}
